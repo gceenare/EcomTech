5255bb43d04ad069887d2f6710d9e9e8359a85ea
gceenare <gcinambabe20@gmail.com>
2025-12-04 06:13:39 +0200

    cc

--- src/main/java/com/curiousity/tech/config/WebConfig.java
+++ src/main/java/com/curiousity/tech/config/WebConfig.java
@@ -1,15 +1,24 @@
 package com.curiousity.tech.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+
     @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        // serve uploaded images from file-system folder 'uploads' under project root
-        registry.addResourceHandler("/uploads/**")
-                .addResourceLocations("file:uploads/");
+    public void configurePathMatch(PathMatchConfigurer configurer) {
+        configurer.addPathPrefix("api", c -> true);
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods("*")
+                .allowedHeaders("*")
+                .allowCredentials(true);
     }
 }
